0598a40bfa6bf26c17ad9f7c8bc6924bb831f88f
Stephen Gbolagade <gbolabyte222@gmail.com>
2024-05-01 13:24:28 +0100

    Added PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+  } = require("next/constants");
+  
+  /** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+  module.exports = async (phase) => {
+    /** @type {import("next").NextConfig} */
+  
+  // Your current or future configuration 
+  
+    const nextConfig = {
+    };
+  
+    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+      const withSerwist = (await import("@serwist/next")).default({
+        swSrc: "public/service-worker/sw.ts",
+        swDest: "public/sw.js",
+        reloadOnOnline: true,
+      });
+      return withSerwist(nextConfig);
+    }
+  
+    return nextConfig;
+  };
+  
